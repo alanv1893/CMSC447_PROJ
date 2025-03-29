9113773358203be87b400768eb204a28b2cd0343
Nihal Patel <nihalhpatel28@gmail.com>
2025-03-29 11:39:37 -0400

    Change to Javascript so deleting the old code

--- auth_app/models.py
+++
@@ -1,6 +0,0 @@
-# Create your models here.
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class CustomUser(AbstractUser):
-    pass  # Add custom fields here if needed
